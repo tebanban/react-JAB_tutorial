95ac7efc0c653fd61219d029dc7811b4e42480d7
tebanban <estebancalf@gmail.com>
2022-02-02 22:35:39 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+import octopuss from './images/octopuss.png';
+import octopuss2 from './images/octopuss2.jpg';
 
 function App() {
+  const refBox=useRef();
+  const exchange=20;
+  
+
+  const sum=(e)=>{
+    console.log(e);
+    e.target.innerText=Number(e.target.innerText) +1;
+    if (e.target.innerText >= 10) {
+      e.target.innerText = 1
+    }
+  }
+
+  const convert=()=>{
+    refBox.current.innerText=Number(refBox.current.innerText)*exchange;
+  }
+
+  const imgChange=(i)=>{
+    i.target.src = octopuss2
+  }
+
+  const textChange=(e)=>{
+    refBox.current.innerText=e.target.value;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input onChange={textChange}/>
+        <div ref={refBox} className='box' onClick={sum}>1</div>
+        <button onClick={convert}>Convert</button>
+        <div><img alt='octo' src={octopuss} onClick={imgChange} className='image' /></div>
+      </header>                            
     </div>
   );
 }
